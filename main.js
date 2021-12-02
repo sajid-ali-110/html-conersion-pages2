ec412eb0a984bc0ad4356eda5cb86f1ef16a93f8
sajid ali <84723005+sajid-ali-110@users.noreply.github.com>
2021-12-02 21:01:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// var	element;
+
+// document.getElementsByClassName("bernard").innerText="<h1>sajid ali</h1>"
+
+// element=document.getElementsByClassName("bernard").innerHTML;
+
+// console.log(element)
+let ele=getElementsByClassName(".sajjuu")
+console.log(ele);
